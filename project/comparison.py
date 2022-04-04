96aeb2769a6b38994d41b0cef7dd4f51148845b9
fdusel <fdusel@student.ubc.ca>
2022-04-03 22:13:01 -0700

    script to compare cmbslow camb planck2018

--- project/comparison.py
+++ project/comparison.py
@@ -22,10 +22,11 @@ Cls = data[0:lmax, 1]
 pars = camb.CAMBparams()
 
 # This function sets up CosmoMC-like settings, with one massive neutrino and helium set using BBN consistency
-om_b = 0.04
-h = 0.675
+om_m = 0.3089
+om_b = 0.0486
+h = 0.6774
 H0 = 100*h
-pars.set_cosmology(H0=H0)  # H0=H0, ombh2=om_b*h**2, omch2=0, mnu=0.06, omk=0, tau=0.06)
+pars.set_cosmology(H0=H0, ombh2=om_b*h**2)  # , omch2=0, mnu=0.06, omk=0, tau=0.06)
 pars.InitPower.set_params(As=2e-9, ns=1, r=0)
 pars.set_for_lmax(lmax-50, lens_potential_accuracy=0)
 
@@ -42,12 +43,12 @@ unlensedCL=powers['unlensed_scalar']
 # Python CL arrays are all zero based (starting at L=0), Note L=0,1 entries will be zero by default.
 # The different CL are always in the order TT, EE, BB, TE (with BB=0 for unlensed scalar results).
 ls = np.arange(totCL.shape[0])
-plt.plot(ls[199:], unlensedCL[199:, 0], color='r', label=r'CAMB for $\Lambda$CDM, unlensed')
+plt.plot(ls[199:], unlensedCL[199:, 0], color='r', label=r'CAMB, unlensed')  # for $\Lambda$CDM, unlensed')
 plt.plot(Ls[199:], Cls[199:], label='PLANCK 2018')
-plt.plot(Ls[199:], C_l(Ls, h=h)[199:]*1000, label='CMBslow')
+plt.plot(Ls[199:], C_l(Ls, h=h)[199:]*1000, label='CMBslow', color='orange')
 plt.xlabel(r'$\ell$')
 plt.ylabel(r'$C_\ell$')
 plt.grid()
 plt.legend()
-plt.savefig(r"C:\Users\Felix\Desktop\cmbslow\figures\comparison.png", transparent=True)
+plt.savefig(fr"C:\Users\Felix\Desktop\cmbslow\figures\comparison om_b={om_b}.png", transparent=True)
 plt.show()
